7ed38f3414f60a54a4ad7306784bd60debcb1a63
Gabriel Morris <gabemorris@outlook.com>
2018-10-11 09:38:22 -0700

    jwt challenge completed with authentication and timeouts

--- src/components/app.js
+++ src/components/app.js
@@ -1,58 +1,114 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {Route, withRouter} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Route, withRouter } from 'react-router-dom';
 
 import HeaderBar from './header-bar';
 import LandingPage from './landing-page';
 import Dashboard from './dashboard';
 import RegistrationPage from './registration-page';
-import {refreshAuthToken} from '../actions/auth';
+import { refreshAuthToken } from '../actions/auth';
+
+import { clearAuth } from '../actions/auth';
+import { clearAuthToken } from '../local-storage';
 
 export class App extends React.Component {
-    componentDidUpdate(prevProps) {
-        if (!prevProps.loggedIn && this.props.loggedIn) {
-            // When we are logged in, refresh the auth token periodically
-            this.startPeriodicRefresh();
-        } else if (prevProps.loggedIn && !this.props.loggedIn) {
-            // Stop refreshing when we log out
-            this.stopPeriodicRefresh();
-        }
+  constructor(props) {
+    super(props);
+
+    (this.countdown = null), (this.alertCounter = null);
+
+    this.resetIdleTimer = this.resetIdleTimer.bind(this);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (!prevProps.loggedIn && this.props.loggedIn) {
+      // When we are logged in, refresh the auth token periodically
+      this.startPeriodicRefresh();
+    } else if (prevProps.loggedIn && !this.props.loggedIn) {
+      // Stop refreshing when we log out
+      this.stopPeriodicRefresh();
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  componentDidMount() {}
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+      () => this.props.dispatch(refreshAuthToken()),
+      36000 // Fifteen minutes
+    );
+  }
+
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return;
     }
 
-    componentWillUnmount() {
-        this.stopPeriodicRefresh();
-    }
+    clearInterval(this.refreshInterval);
+  }
 
-    startPeriodicRefresh() {
-        this.refreshInterval = setInterval(
-            () => this.props.dispatch(refreshAuthToken()),
-            60 * 60 * 1000 // One hour
-        );
-    }
+  logOut() {
+    this.props.dispatch(clearAuth());
+    clearAuthToken();
+  }
 
-    stopPeriodicRefresh() {
-        if (!this.refreshInterval) {
-            return;
-        }
+  idleLogoutTimer() {
+    clearTimeout(this.countdown);
+    clearTimeout(this.alertCounter);
+    console.log('timer started');
 
-        clearInterval(this.refreshInterval);
-    }
+    this.countdown = setTimeout(() => this.logOut(), 18000000);
 
-    render() {
-        return (
-            <div className="app">
-                <HeaderBar />
-                <Route exact path="/" component={LandingPage} />
-                <Route exact path="/dashboard" component={Dashboard} />
-                <Route exact path="/register" component={RegistrationPage} />
-            </div>
-        );
-    }
+    this.alertCounter = setTimeout(() => {
+      const stayOnPage = window.confirm('Session is about to expire');
+      console.log('hurp', stayOnPage);
+
+      if (!stayOnPage) {
+        console.log('clearing timeout');
+        clearTimeout(this.countdown);
+        clearTimeout(this.alertCounter);
+        this.resetIdleTimer();
+      }
+    }, 14400000);
+  }
+
+  resetIdleTimer() {
+    clearTimeout(this.countdown);
+    console.log('resetting idle timer');
+    this.idleLogoutTimer();
+  }
+
+  render() {
+    return (
+      <div
+        className="app"
+        onLoad={this.idleLogoutTimer()}
+        onClick={() => {
+          console.log('clicked');
+          this.resetIdleTimer();
+        }}
+      >
+        <HeaderBar />
+        <Route exact path="/" component={LandingPage} />
+        <Route
+          exact
+          path="/dashboard"
+          component={Dashboard}
+          stayLoggedIn={this.resetIdleTimer}
+        />
+        <Route exact path="/register" component={RegistrationPage} />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    hasAuthToken: state.auth.authToken !== null,
-    loggedIn: state.auth.currentUser !== null
+  hasAuthToken: state.auth.authToken !== null,
+  loggedIn: state.auth.currentUser !== null
 });
 
 // Deal with update blocking - https://reacttraining.com/react-router/web/guides/dealing-with-update-blocking
--- src/index.js
+++ src/index.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom';
-import {Provider} from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './components/app';
 import store from './store';
 import './index.css';
 
 ReactDOM.render(
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
-    document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root')
 );
+
+// Automatically log the user out after five minutes of inactivity.
+
+// Show a dialog box one minute before the user is automatically logged out, allowing them to click a button to indicate that the are still using the application.
+
+/*
+- when app minutes
+- Click listener for the whole page
+- wait five minutes without a click
+- perform logout logic
+*/
